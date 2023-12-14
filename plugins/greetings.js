9240d5f84b9d9f2866992cd9a0d15d29e1b0c320
edwin cg <94446732+macmelcreations@users.noreply.github.com>
2023-12-13 23:46:43 -0800

    Update greetings.js

--- plugins/greetings.js
+++ plugins/greetings.js
@@ -30,7 +30,7 @@ System({
             let replyMsg = `Welcome manager\n\nGroup: ${
             (await message.client.groupMetadata(message.jid)).subject
             }\nStatus: ${stat}\n\nAvailable Actions:\n\n- ${prefix}welcome get: Get the welcome message\n- ${prefix}welcome on: Enable welcome message\n- ${prefix}welcome off: Disable welcome message\n- ${prefix}welcome delete: Delete the welcome message`;
-
+            return await message.client.sendMessage(message.chat, {text: `_@${jid[0].split("@")[0]} promoted as admin successfully_`, mentions: jid, });
             return await message.reply(replyMsg);
         }
 
